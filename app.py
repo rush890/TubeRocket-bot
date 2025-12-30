592d9ae20d1f1a5120d66834ee4a88066724e25c
rush890 <131534599+rush890@users.noreply.github.com>
2025-12-30 12:22:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+import subprocess, time, requests,random
+
+# List of passwords
+user_passwords = [
+    '9ad14420a63411ee8dec29e7b2d7d8ec', 'b0115460a63411ee8dec29e7b2d7d8ec',
+    'cf203a10a63411ee8dec29e7b2d7d8ec', 'b0521f80a4d711ee8dec29e7b2d7d8ec',
+    'f9ea0dc0a63411ee8dec29e7b2d7d8ec', '21f550d0a53c11ee8dec29e7b2d7d8ec',
+    '3ac6da30a93711ee8dec29e7b2d7d8ec', '37ec0070afbc11eeb6ec9f4e674dc15b',
+    '2709d2d0b4f011eeb6ec9f4e674dc15b', 'a00c4220b4f111eeb6ec9f4e674dc15b',
+    '7f60c8a0b4df11eeb6ec9f4e674dc15b', 'ed23c840b4f011eeb6ec9f4e674dc15b',
+    '51470170b4f111eeb6ec9f4e674dc15b', 'ac15ba4047f811ed9bb601573df1ee27',
+    '429235c0d1ca11eca19befd8b74558ba', 'f3161e40a63311ee8dec29e7b2d7d8ec',
+    '397a7fb0d1e811eca462957b5204fdf1'
+]
+print("Getting proxies")
+
+
+def save_proxies_to_file(proxies, filename='proxies.txt'):
+  try:
+    with open(filename, 'w') as file:
+      for proxy in proxies:
+        if(proxy):
+          file.write(f"{proxy}")
+    print(f"Proxies saved successfull Xy to {filename}")
+  except Exception as e:
+    print(f"Error: {e}")
+
+
+def savepr():
+  api_url = "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&protocol=http&anonymity=all&timeout=40&proxy_format=protocolipport&format=text"
+  prx = []
+  print("starting")
+  pr = requests.get(api_url).text
+  pr = pr.split("\n")
+  print("Found proxies", len(pr))
+  save_proxies_to_file(pr)
+
+
+def get_random_wait_time():
+  return random.uniform(30 * 60, 60 * 60)
+
+
+print("starting")
+# Run process_password function for each password in a separate process
+while True:
+  savepr()
+  processes = []
+  for password in user_passwords:
+    command = ['python', 'tube_all.py', password]
+    process = subprocess.Popen(command)
+    processes.append(process)
+    time.sleep(10)
+
+  # Generate a random wait time
+  wait_time = get_random_wait_time()
+
+  print(f"Waiting for {wait_time / 60:.2f} minutes before stopping processes.")
+  time.sleep(wait_time)
+
+  # Stop all processes
+  for process in processes:
+    process.terminate()
+
+  print("Processes stopped. Restarting.")
---
+++ tube_all.py
@@ -0,0 +1,128 @@
+import time
+import requests
+import random
+import sys
+
+
+def read_proxies(file_path):
+  with open(file_path, 'r') as file:
+    proxies = [line.strip() for line in file.readlines()]
+  return proxies
+
+
+def save_proxies(file_path, proxies):
+  with open(file_path, 'w') as file:
+    for proxy in proxies:
+      file.write(proxy + '\n')
+
+
+def get_random_proxy():
+  proxy_file="proxies.txt"
+  proxies = read_proxies(proxy_file)
+
+  if not proxies:
+    print("No proxies available.")
+    return
+
+  # Randomly pick a proxy
+  chosen_proxy = random.choice(proxies)
+
+  # Remove the chosen proxy from the list
+  proxies.remove(chosen_proxy)
+  # Save the updated list of proxies back to the file
+  save_proxies(proxy_file, proxies)
+  # print(f"Processing with proxy: {chosen_proxy}")
+  return chosen_proxy
+
+
+def countdown(seconds):
+  for j in range(seconds, 0, -1):
+    #print('Waiting for', j, 'seconds ', end='\r')
+    time.sleep(1)
+
+BASE_URL="http://mutupipe.westus2.cloudapp.azure.com:3000/api/"
+def get_token(to,pr):
+  url = BASE_URL+'version-check'
+  ver = str(requests.get(url=url, proxies=pr).json()['result']['version_android'])
+  url = BASE_URL+'signIn'
+  head = {'token': to, 'versionCode': ver}
+  return requests.post(url=url, headers=head, proxies=pr).json()
+
+
+def get_video_info(to,pr):
+  url = BASE_URL+'video'
+  head = {'token': to}
+  dl = requests.get(url=url, headers=head, proxies=pr).json()
+  #print(dl)
+  return dl['result']['videoId'], dl['result']['playSecond']
+
+
+def receive_reward(to, video_id,pr):
+  url = BASE_URL+'video'
+  head = {'token': to, 'Content-Type': 'application/json; charset=UTF-8'}
+  data = '{"id":"' + video_id + '","playCount":0,"playSecond":0,"boost":0,"status":""}'
+  res = requests.put(url=url, headers=head, data=data, proxies=pr).json()
+  print(res)
+  return res['result']['coin']
+
+
+cr_sum = 0
+
+
+def process_password(password,pr):
+  global cr_sum
+  err_count = 0
+
+  while True:
+    try:
+      to = get_token(password,pr)['result']['token']
+      coin = (requests.get(url=BASE_URL+'member',
+                           headers={
+                               'token': to
+                           }, proxies=pr).json()['result']['coin'])
+      print(coin)
+      while True:
+        try:
+          video_id, tg = get_video_info(to,pr)
+          print("watching for", tg)
+          countdown(tg + 1)
+          nwcoin = receive_reward(to, video_id,pr)
+          try:
+            cr_sum += (nwcoin - coin)
+            print("Coins Earned are *****:", nwcoin - coin)
+            print("Total Coined earned in this cycle:", cr_sum)
+            coin = nwcoin
+          except:
+            print("Error in coin calculation")
+        except Exception as e:
+          err_count += 1
+          if (err_count > 15):
+            print("User cooldown!!!!!!!!!!", password)
+            err_count = 0
+            time.sleep(5000)
+          #print("Error in get_video_info", e)
+          time.sleep(random.randint(200, 270))
+          break
+    except Exception as e:
+      print("Error in get_token",e)
+      time.sleep(150)
+
+
+
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: python your_script.py <password>")
+    sys.exit(1)
+
+  password_to_process = sys.argv[1]
+  proxy_string=get_random_proxy()
+
+  protocol, proxy = proxy_string.split("://")
+  host, port = proxy.split(":")
+  # host=host[6:]
+  pr = {
+      protocol: f"{host}:{port}",
+  }
+  print("proxy:",pr)
+  process_password(password_to_process,pr)
